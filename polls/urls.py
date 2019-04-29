08e6e2ad6c81c1d7e0b5825a6019d77d417c8ae3
stepbystep <likerberi97@gmail.com>
2019-04-29 15:36:48 +0900

    generic view part2

--- polls/urls.py
+++ polls/urls.py
@@ -4,11 +4,12 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    # ex2: path('', views.IndexView, name='index'), .as_view() added. 
+    path('', views.IndexView.as_view(), name='index'),
     # ex: pk=question_id -> /polls/5/
-    path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results
-    path('<int:pk>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
     # ex: /polls/5/vote
     path('<int:question_id/vote/', views.vote, name='vote'),
     
--- polls/views.py
+++ polls/views.py
@@ -73,3 +73,8 @@ def vote(request, question_id):
 
 
     return HttpResponse("You are voting on question %s" % question_id)
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    
